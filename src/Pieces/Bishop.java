0fcca98a933a68cdedcbe3f36edf57b4e694f247
jasminebuttar <jb1620@scarletmail.rutgers.edu>
2018-10-29 23:20:15 -0400

    bishop

--- src/Pieces/Bishop.java
+++ src/Pieces/Bishop.java
@@ -1,5 +1,32 @@
 package Pieces;
+import Pieces.Allpieces;
+import app.Position;
 
-public class Bishop {
-//worked
+public class Bishop extends Allpieces{
+	
+	public Bishop(char color){
+		super(color);
+		this.type = "BISHOP";
+	}
+		
+    public char getColor(){
+		return color;
+	}
+		
+    public String toString(){
+		return super.toString() + "B";
+	}
+		
+	public boolean validMove(Position start, Position end, char s, boolean path){
+		boolean check = super.validMove(start, end, s, path);
+			
+		if(check && path == false){
+			if(start.isDiagonal(end)){
+				return true;
+			}
+		}
+		return false;
+	}
 }
+
+
