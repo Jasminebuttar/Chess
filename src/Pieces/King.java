fc1bd8c5531a7f17c68d63eec1a921f321862a62
jasminebuttar <jb1620@scarletmail.rutgers.edu>
2018-10-29 23:31:48 -0400

    updated rook king

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -1,5 +1,31 @@
 package Pieces;
 
-public class King {
+import app.Position;
 
+public class King extends Allpieces{
+	
+	public King(char color){
+		super(color);
+		this.type = "KING";
+	}
+	
+	public char getColor(){
+		return color;
+	}
+	
+	public String toString(){
+		return super.toString() + "K";
+	}
+	
+	public boolean validMove(Position start, Position end, char s, boolean path){
+		boolean validityCheck = super.validMove(start, end, s, path);
+		
+		if(validityCheck && path == false){
+			if(start.isAdjacent(end)){
+				return true;
+			}
+		}
+		return false;
+	}
 }
+
---
+++ src/Pieces/Rook.java
@@ -0,0 +1,33 @@
+package Pieces;
+import app.Position;
+
+public class Rook extends Allpieces{
+	
+	public Rook(char color){
+		super(color);
+		this.type = "ROOK";
+	}
+	
+	
+	public char getColor(){
+		return color;
+	}
+	
+	public String toString(){
+		return super.toString() + "R";
+	}
+	
+	public boolean validMove(Position start, Position end, char s, boolean path){
+		boolean check = super.validMove(start, end, s, path);
+		
+		if(check && path == false){
+			if(start.vert == end.vert){
+				return true;
+			}else if(start.horz == end.horz){
+				return true;
+			}	
+		}
+		return false;
+	}
+}
+
